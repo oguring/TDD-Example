87efc84e526cc7c2067935ba2621dd92f0ef98c0
tamamo-c <tamac523@gmail.com>
2022-11-02 06:00:14 +0900

    Add negative tests for TypeError

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,3 +1,5 @@
+import pytest
+
 from calculator.math import Calculator
 
 
@@ -5,3 +7,15 @@ def test_min():
     calc = Calculator()
     assert calc.min(1, 2) == 1
     assert calc.min(2, 1) == 1
+
+
+def test_min_negative():
+    calc = Calculator()
+    with pytest.raises(TypeError):
+        calc.min("1", 2)
+
+    with pytest.raises(TypeError):
+        calc.min(3.4, 2)
+
+    with pytest.raises(TypeError):
+        calc.min("1", "2")
