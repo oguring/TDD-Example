8332036afc8132f372bff5814eb5d7216380f98d
tamamo-c <tamac523@gmail.com>
2022-11-02 06:04:36 +0900

    Add Type checking

--- calculator/math.py
+++ calculator/math.py
@@ -1,3 +1,5 @@
 class Calculator:
-    def min(self, left_val, right_val):
+    def min(self, left_val: int, right_val: int):
+        if not isinstance(left_val, int) or not isinstance(right_val, int):
+            raise TypeError
         return left_val if left_val < right_val else right_val
